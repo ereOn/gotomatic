250522958411be1847a7a8c59afb277f18afcfe0
Julien Kauffmann <julien.kauffmann@freelan.org>
2017-03-09 06:31:41 -0500

    Perfected coverage

--- conditional/composite_test.go
+++ conditional/composite_test.go
@@ -2,6 +2,16 @@ package conditional
 
 import "testing"
 
+func TestCompositeConditionNoSubconditions(t *testing.T) {
+	defer func() {
+		if err := recover(); err == nil {
+			t.Fatalf("instantiation was supposed to panic")
+		}
+	}()
+
+	NewCompositeCondition(OperatorAnd)
+}
+
 func TestCompositeConditionOperatorAnd(t *testing.T) {
 	a := NewManualCondition(false)
 	b := NewManualCondition(false)
@@ -22,6 +32,60 @@ func TestCompositeConditionOperatorAnd(t *testing.T) {
 
 	a.Set(false)
 	assertConditionState(t, condition, false, "a set to false")
+
+	assertConditionChanged(t, condition, false, "a set to true again", func() { a.Set(true) })
+}
+
+func TestCompositeConditionOperatorOr(t *testing.T) {
+	a := NewManualCondition(false)
+	b := NewManualCondition(false)
+
+	condition := NewCompositeCondition(OperatorOr, a, b)
+	defer condition.Close()
+	assertConditionState(t, condition, false, "initialization to: and, (false, false)")
+
+	a.Set(true)
+	assertConditionState(t, condition, true, "a set to true")
+
+	b.Set(true)
+	assertConditionState(t, condition, true, "b set to true")
+
+	condition = NewCompositeCondition(OperatorOr, a, b)
+	defer condition.Close()
+	assertConditionState(t, condition, true, "initialization to: and, (true, true)")
+
+	a.Set(false)
+	assertConditionState(t, condition, true, "a set to false")
+
+	b.Set(false)
+	assertConditionState(t, condition, false, "b set to false")
+
+	assertConditionChanged(t, condition, false, "a set to true again", func() { a.Set(true) })
+}
+
+func TestCompositeConditionOperatorXor(t *testing.T) {
+	a := NewManualCondition(false)
+	b := NewManualCondition(false)
+
+	condition := NewCompositeCondition(OperatorXor, a, b)
+	defer condition.Close()
+	assertConditionState(t, condition, false, "initialization to: and, (false, false)")
+
+	a.Set(true)
+	assertConditionState(t, condition, true, "a set to true")
+
+	b.Set(true)
+	assertConditionState(t, condition, false, "b set to true")
+
+	condition = NewCompositeCondition(OperatorXor, a, b)
+	defer condition.Close()
+	assertConditionState(t, condition, false, "initialization to: and, (true, true)")
+
+	a.Set(false)
+	assertConditionState(t, condition, true, "a set to false")
+
+	b.Set(false)
+	assertConditionState(t, condition, false, "b set to false")
 
 	assertConditionChanged(t, condition, false, "a set to true again", func() { a.Set(true) })
 }
