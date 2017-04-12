e6d07e639e2359762cba1b8d075ea56220757b36
Julien Kauffmann <julien.kauffmann@freelan.org>
2017-04-12 08:46:41 +0200

    Implemented recurrentMoment

--- conditional/time.go
+++ conditional/time.go
@@ -590,5 +590,26 @@ func (f frequencySecond) Next(start time.Time, t time.Time) time.Time {
 }
 
 // Moment represents a moment in time.
-type Moment struct {
+type Moment interface {
+	NextInterval(time.Time) (bool, time.Time)
+}
+
+type recurrentMoment struct {
+	Start     time.Time
+	Stop      time.Time
+	Frequency Frequency
+}
+
+// NextInterval returns a boolean flag that indicates whether the specified
+// time is within the interval, and the time of the next interval boundary.
+func (r recurrentMoment) NextInterval(t time.Time) (bool, time.Time) {
+	previousStart := r.Frequency.Previous(r.Start, t)
+	nextStart := r.Frequency.Next(r.Start, t)
+	currentStop := r.Frequency.Next(r.Stop, previousStart)
+
+	if currentStop.Before(t) {
+		return false, nextStart
+	}
+
+	return true, currentStop
 }
--- conditional/time_test.go
+++ conditional/time_test.go
@@ -782,3 +782,43 @@ func TestFrequencySecond(t *testing.T) {
 		t.Errorf("expected: %s, got: %s", expected, value)
 	}
 }
+
+func TestRecurrentMoment(t *testing.T) {
+	edt, err := time.LoadLocation("Canada/Eastern")
+
+	if err != nil {
+		panic(err)
+	}
+
+	moment := recurrentMoment{
+		Start:     time.Date(1990, 1, 1, 1, 15, 10, 0, edt),
+		Stop:      time.Date(1990, 1, 1, 1, 40, 10, 0, edt),
+		Frequency: FrequencyHour,
+	}
+
+	now := time.Date(2017, 4, 10, 15, 22, 0, 0, edt)
+	expectedState := true
+	expectedTime := time.Date(2017, 4, 10, 15, 40, 10, 0, edt)
+	state, vtime := moment.NextInterval(now)
+
+	if expectedState != state {
+		t.Errorf("expected: %t, got: %t", expectedState, state)
+	}
+
+	if expectedTime != vtime {
+		t.Errorf("expected: %s, got: %s", expectedTime, vtime)
+	}
+
+	now = time.Date(2017, 4, 10, 15, 42, 0, 0, edt)
+	expectedState = false
+	expectedTime = time.Date(2017, 4, 10, 16, 15, 10, 0, edt)
+	state, vtime = moment.NextInterval(now)
+
+	if expectedState != state {
+		t.Errorf("expected: %t, got: %t", expectedState, state)
+	}
+
+	if expectedTime != vtime {
+		t.Errorf("expected: %s, got: %s", expectedTime, vtime)
+	}
+}
